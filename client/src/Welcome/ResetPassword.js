76620e2c0bde04c6b22e29a610d163224d8926e9
Robin Liu <robin.liu.mr@gmail.com>
2022-09-02 16:33:53 +0200

    style of password reset

--- client/src/Welcome/ResetPassword.js
+++ client/src/Welcome/ResetPassword.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-import StepEmail from "./StepEmail";
+import Step from "./Step";
 
 export default class ResetPassword extends Component {
     constructor(props) {
@@ -85,13 +85,10 @@ export default class ResetPassword extends Component {
         if (stepState === 1) {
             stepDisplay = (
                 <div>
-                    <StepEmail />
-                    {this.state.error && (
-                        <p className="error">{this.state.error}</p>
-                    )}
+                    <Step step="email" />
 
-                    <div className="max-w-2xl mx-auto">
-                        <p className="ml-0.5 mb-4 text-lg">
+                    <div className="max-w-xl mx-auto">
+                        <p className="ml-1 mb-4 text-lg">
                             Please enter the email with which you registered.
                         </p>
                         <form
@@ -128,44 +125,90 @@ export default class ResetPassword extends Component {
                                 </svg>
                             </button>
                         </form>
+
+                        {this.state.error && (
+                            <p className="error">{this.state.error}</p>
+                        )}
                     </div>
                 </div>
             );
         }
         if (stepState === 2) {
             stepDisplay = (
-                <div>
-                    Reset Password
+                <div className="max-w-xl mx-auto">
+                    <Step step="verify" />
+
+                    <p className="ml-1 mb-4 text-lg">
+                        Please enter the code you received
+                    </p>
+                    <form
+                        key={stepState}
+                        onSubmit={this.onVerifySubmit}
+                        className="relative"
+                    >
+                        <label className="sr-only" htmlFor="code">
+                            {" "}
+                            Code{" "}
+                        </label>
+                        <input
+                            className="w-full py-4 pl-3 pr-16 text-sm border-2 border-gray-200 rounded-lg"
+                            id="code"
+                            type="code"
+                            name="code"
+                            required
+                            placeholder="Code"
+                        />
+
+                        <label className="sr-only" htmlFor="password">
+                            {" "}
+                            Password{" "}
+                        </label>
+                        <input
+                            className="w-full py-4 pl-3 pr-16 text-sm border-2 border-gray-200 rounded-lg"
+                            id="password"
+                            type="password"
+                            name="password"
+                            required
+                            placeholder="Password"
+                        />
+                        <button className="absolute p-2 text-white bg-blue-600 rounded-full -translate-y-1/2 top-1/2 right-4">
+                            <svg
+                                className="w-4 h-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                                />
+                            </svg>
+                        </button>
+                    </form>
+
                     {this.state.error && (
                         <p className="error">{this.state.error}</p>
                     )}
-                    <p>Please enter the code you received</p>
-                    <form key={stepState} onSubmit={this.onVerifySubmit}>
-                        <label>
-                            Code:
-                            <input
-                                id="code"
-                                name="code"
-                                placeholder="Code"
-                            ></input>
-                        </label>
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                        ></input>
-                        <button>Submit</button>
-                    </form>
                 </div>
             );
         }
         if (stepState === 3) {
             stepDisplay = (
                 <div>
-                    Reset Password
-                    <p>Success!</p>
-                    You can now <Link to="/login">log in</Link> with your new
-                    password.
+                    <Step step="success" />
+                    <div className="max-w-xl mx-auto">
+                        <p className="text-center mb-4 text-lg">Success!</p>
+                        <p className="text-sm text-center">
+                            You can now{" "}
+                            <Link className="underline" to="/">
+                                log in
+                            </Link>{" "}
+                            with your new password.
+                        </p>
+                    </div>
                 </div>
             );
         }
